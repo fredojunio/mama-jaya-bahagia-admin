6e1eb04cb836ede98d77c9d6805ce5cc5b903b05
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-06-02 15:53:51 +0700

    fix: rit - jual barang - validation for kiriman not counting the bought tonnage

--- src/views/admin/rit/jual_barang_revisi.vue
+++ src/views/admin/rit/jual_barang_revisi.vue
@@ -644,7 +644,12 @@
                   type="button"
                   :disabled="
                     selectedCustomer.type == 'Kiriman' &&
-                    newTransaction.rits.some((rit) => rit.real_tonnage < 0)
+                    (newTransaction.rits.some((rit) => rit.real_tonnage < 0) ||
+                      newTransaction.rits.some(
+                        (rit) =>
+                          rit.real_tonnage <
+                          rit.item.tonnage_left - 50 - rit.tonnage
+                      ))
                   "
                   class="disabled:opacity-50 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-black text-base font-medium text-white hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black sm:ml-3 sm:w-auto sm:text-sm"
                   @click.once="createData()"
