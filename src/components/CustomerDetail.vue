a04546e3c8d4327079594aa3728d18f259f04473
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-06-14 19:45:22 +0700

    feat: customer - hide deposit tabungan button on mobile

--- src/components/CustomerDetail.vue
+++ src/components/CustomerDetail.vue
@@ -28,13 +28,13 @@
                 >
                   Approve Cashback
                 </button>
-                <button
+                <!-- <button
                   v-if="$route.path == '/admin/customer'"
                   @click="showDepositSavingsForm = true"
                   class="inline-flex items-center justify-center rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:opacity-90 focus:ring-offset-2 sm:w-auto"
                 >
                   Deposit Tabungan
-                </button>
+                </button> -->
                 <button
                   v-if="$route.path == '/admin/customer'"
                   @click="showWithdrawSavingsForm = true"
