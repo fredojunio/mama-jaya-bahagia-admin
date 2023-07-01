27522de725b685fdec3fcebb4e0ebb6681c1829e
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-07-01 09:59:26 +0700

    fix: pengeluaran - add form can't be opened

--- src/views/admin/finance/index.vue
+++ src/views/admin/finance/index.vue
@@ -38,7 +38,7 @@
           />
         </div>
         <button
-          @click="showEditExpenseForm = true"
+          @click="showAddExpenseForm = true"
           class="inline-flex items-center justify-center rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:opacity-90 focus:ring-offset-2 sm:w-auto"
         >
           Tambah Pengeluaran
