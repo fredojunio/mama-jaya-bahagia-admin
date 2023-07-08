25581f33c12f4f0ce813c7a8acf2af690756e910
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-07-08 21:04:27 +0700

    fix: rit detail not showing

--- src/views/admin/rit/index.vue
+++ src/views/admin/rit/index.vue
@@ -289,7 +289,10 @@
                           <span class="ml-3">Detail</span>
                         </div>
                       </div>
-                      <div v-if="role_id == 1" class="flex flex-col items-start">
+                      <div
+                        v-if="role_id == 1"
+                        class="flex flex-col items-start"
+                      >
                         <div
                           @click="openRitHistory(rit.id)"
                           class="cursor-pointer relative flex-1 inline-flex items-center justify-between text-sm text-gray-500 font-medium border border-transparent rounded-bl-lg hover:text-black group/edit"
@@ -819,7 +822,10 @@
                           <span class="ml-3">Detail</span>
                         </div>
                       </div>
-                      <div v-if="role_id == 1" class="flex flex-col items-start">
+                      <div
+                        v-if="role_id == 1"
+                        class="flex flex-col items-start"
+                      >
                         <div
                           @click="openRitHistory(rit.id)"
                           class="cursor-pointer relative flex-1 inline-flex items-center justify-between text-sm text-gray-500 font-medium border border-transparent rounded-bl-lg hover:text-black group/edit"
@@ -2222,6 +2228,240 @@
     </TransitionRoot>
     <!-- //!SECTION -->
     <!-- //SECTION Detail -->
+    <TransitionRoot as="template" :show="showRitDetail">
+      <Dialog
+        as="div"
+        class="fixed z-10 inset-0 overflow-y-auto"
+        @close="showRitDetail = false"
+      >
+        <div
+          class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+        >
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0"
+            enter-to="opacity-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100"
+            leave-to="opacity-0"
+          >
+            <DialogOverlay
+              class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+            />
+          </TransitionChild>
+
+          <!-- This element is to trick the browser into centering the modal contents. -->
+          <span
+            class="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+            >&#8203;</span
+          >
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100 translate-y-0 sm:scale-100"
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div
+              class="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-5xl sm:w-full sm:p-6"
+            >
+              <form class="space-y-8 divide-y divide-gray-200">
+                <div class="space-y-8 divide-y divide-gray-200">
+                  <div>
+                    <div>
+                      <h3 class="text-lg leading-6 font-medium text-gray-900">
+                        Riwayat Penjualan
+                      </h3>
+                    </div>
+                    <hr />
+                    <form class="mt-6 flex space-x-4" action="#">
+                      <div class="flex-1 min-w-0">
+                        <label for="search" class="sr-only">Search</label>
+                        <div class="relative rounded-md shadow-sm">
+                          <div
+                            class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+                          >
+                            <Icon
+                              icon="uil:search"
+                              class="h-5 w-5 text-gray-400"
+                            />
+                          </div>
+                          <input
+                            v-model="searchRitDetailQuery"
+                            type="search"
+                            name="search"
+                            id="search"
+                            class="focus:ring-pink-500 focus:border-pink-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+                            placeholder="Search"
+                          />
+                        </div>
+                      </div>
+                    </form>
+                    <div
+                      class="max-w-7xl grid grid-cols-1 mx-auto mb-8 gap-x-4"
+                    >
+                      <div class="mt-8 flex flex-col">
+                        <div
+                          class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8"
+                        >
+                          <div
+                            class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8"
+                          >
+                            <div
+                              class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg"
+                            >
+                              <table
+                                class="min-w-full divide-y divide-gray-300"
+                              >
+                                <thead class="bg-gray-50">
+                                  <tr>
+                                    <th
+                                      scope="col"
+                                      class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                                    >
+                                      Customer
+                                    </th>
+                                    <th
+                                      scope="col"
+                                      class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                                    >
+                                      Tanggal
+                                    </th>
+                                    <th
+                                      scope="col"
+                                      class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                                    >
+                                      Tonase (kg)
+                                    </th>
+                                    <th
+                                      scope="col"
+                                      class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                                    >
+                                      Total Penjualan
+                                    </th>
+                                    <th
+                                      scope="col"
+                                      class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                                    >
+                                      Sisa Tonase
+                                    </th>
+                                  </tr>
+                                </thead>
+                                <tbody
+                                  class="divide-y divide-gray-200 bg-white"
+                                >
+                                  <tr
+                                    v-for="ritTransaction in selectedData.transactions.filter(
+                                      (transaction) =>
+                                        transaction.transaction
+                                          .owner_approved == 1 &&
+                                        (searchRitDetailQuery
+                                          ? transaction.customer_name
+                                              .toLowerCase()
+                                              .includes(searchRitDetailQuery)
+                                          : true)
+                                    )"
+                                    :key="ritTransaction.id"
+                                  >
+                                    <td
+                                      class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6 grow"
+                                    >
+                                      <div class="flex items-center">
+                                        <div class="font-medium text-gray-900">
+                                          {{ ritTransaction.customer_name }}
+                                        </div>
+                                      </div>
+                                    </td>
+                                    <td
+                                      class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6 grow"
+                                    >
+                                      <div class="flex items-center">
+                                        <div class="font-medium text-gray-900">
+                                          {{
+                                            formatDate(
+                                              ritTransaction.created_at
+                                            )
+                                          }}
+                                        </div>
+                                      </div>
+                                    </td>
+                                    <td
+                                      class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6 grow"
+                                    >
+                                      <div class="flex items-center">
+                                        <div class="font-medium text-gray-900">
+                                          {{
+                                            formatNumber(
+                                              ritTransaction.tonnage *
+                                                ritTransaction.masak
+                                            )
+                                          }}
+                                          kg
+                                        </div>
+                                      </div>
+                                    </td>
+                                    <td
+                                      class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6 grow"
+                                    >
+                                      <div class="flex items-center">
+                                        <div class="font-medium text-gray-900">
+                                          Rp.
+                                          {{
+                                            formatNumber(
+                                              ritTransaction.total_price
+                                            )
+                                          }}
+                                        </div>
+                                      </div>
+                                    </td>
+                                    <td
+                                      class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6 grow"
+                                    >
+                                      <div class="flex items-center">
+                                        <div class="font-medium text-gray-900">
+                                          {{
+                                            formatNumber(
+                                              ritTransaction.tonnage_left
+                                            )
+                                          }}
+                                          kg
+                                        </div>
+                                      </div>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="pt-5">
+                  <div class="flex justify-end">
+                    <button
+                      type="button"
+                      @click="showRitDetail = false"
+                      class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </TransitionChild>
+        </div>
+      </Dialog>
+    </TransitionRoot>
+    <!-- //!SECTION -->
+    <!-- //SECTION History -->
     <TransitionRoot as="template" :show="showRitHistory">
       <Dialog
         as="div"
