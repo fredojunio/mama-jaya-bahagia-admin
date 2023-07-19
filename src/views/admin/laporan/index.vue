df0d2591ce0c14bf0787957d6b5bf4225a09138d
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-07-19 23:35:01 +0700

    feat: report - replace daily report

--- src/views/admin/laporan/index.vue
+++ src/views/admin/laporan/index.vue
@@ -46,13 +46,6 @@
           <div class="flex items-center justify-between">
             <h2 class="text-lg font-medium text-gray-900">Directory</h2>
             <button
-              v-if="
-                reports.every(
-                  (report) =>
-                    report.created_at.substring(0, 10) !=
-                    new Date().toISOString().substring(0, 10)
-                ) || reports.length <= 0
-              "
               @click="showSaveDailyReportForm = true"
               class="inline-flex items-center justify-center rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:opacity-90 focus:ring-offset-2 sm:w-auto"
             >
