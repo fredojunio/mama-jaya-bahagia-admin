b4a78884b828030a688d21e0b0159e3766a5636b
akristianto03 <ezralfredo@gmail.com>
2024-06-30 02:36:10 +0700

    Update index.vue

--- src/views/admin/laporan/index.vue
+++ src/views/admin/laporan/index.vue
@@ -44,7 +44,7 @@
       >
         <div class="px-6 pt-6 pb-4">
           <div class="flex items-center justify-between">
-            <h2 class="text-lg font-medium text-gray-900">Directory</h2>
+            <h2 class="text-lg font-medium text-gray-900">Diretory</h2>
             <button
               @click="showSaveDailyReportForm = true"
               class="inline-flex items-center justify-center rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:opacity-90 focus:ring-offset-2 sm:w-auto"
